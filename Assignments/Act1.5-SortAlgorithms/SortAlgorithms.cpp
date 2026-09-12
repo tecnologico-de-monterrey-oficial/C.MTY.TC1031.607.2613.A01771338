58b978adbaee0cef08581cbf6020a1bf287fcd69
ElenaaJordan <A01771338@tec.mx>
2026-09-11 21:38:40 -0600

    update studiying

--- Assignments/Act1.5-SortAlgorithms/SortAlgorithms.cpp
+++ Assignments/Act1.5-SortAlgorithms/SortAlgorithms.cpp
@@ -2,10 +2,38 @@
 //A01771338
 
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
 using namespace std;
+
+
+
+int comparaciones = 0;
+int intercambios = 0;
+
+//nuevo algoritmo para ordenar 
+template <typename T>
+void shellSort(vector<T> &list) {
+    int size = list.size(); // Obtenemos el tamaño de la lista
+    for (int gap = size/2; gap > 0; gap = gap/2) { // para cada intervalo hasta que gap < 1
+        for (int i = gap; i < size; i++) {
+            T temp = list[i]; // Guardamos el valor actual
+            int j = i; // Inicializamos j con el valor de i
+            while (j >= gap && list[j-gap] > temp) { // Comparamos el valor actual con el de la posición anterior
+                list[j] = list[j-gap]; // Desplazamos el elemento hacia la derecha
+                j = j - gap; // Actualizamos j
+            }
+            list[j] = temp; // Colocamos el valor actual en la posición correcta
+        }
+    }
+}
 
 template <typename T>
 void swap(vector<T> &list, int i, int j) {
+    comparaciones++; 
     if (i != j) {
         // creamos una variable temporal
         T aux = list[i];
@@ -18,13 +46,13 @@ void swap(vector<T> &list, int i, int j) {
 
 template <typename T>
 void swapSort(vector<T> &list) {
-    // iteramos todos los elementos de la lista hasta el penultimo
     for (int i=0; i < list.size()-1; i++) {
         for (int j=i+1; j<list.size(); j++) {
-            // comparamos el valor de j con el valor de i para determinar si es menor
+
+            comparaciones++;
             if (list[j] < list[i]) {
-                // si es menor
-                // intercambiamos los valores
+
+                intercambios++;
                 swap(list, i, j);
             }
         }
@@ -33,99 +61,106 @@ void swapSort(vector<T> &list) {
 
 template <typename T>
 void bubbleSort(vector<T> &list) {
-    // definimos un booleano como verdadero
-    bool change = true;
-    // iterar desde n hasta 1
-    for (int i=list.size()-1; i>0 && change; i--) {
-        // cambio el valor de change a falso
-        change = false;
-        // iteramos desde 0 hasta qu sea menor que i
+
+    bool change = true;  // definimos un booleano como verdadero
+    
+    for (int i=list.size()-1; i>0 && change; i--) { // iterar desde n hasta 1
+
+        change = false; // cambio el valor de change a falso para que no se salga del ciclo
+    
         for (int j=0; j<i; j++) {
-            // comparamos el valor de j con el valor de j+1 para determinar si es mayor
+    
+
+            comparaciones++;
             if (list[j] > list[j+1]) {
-                // si es mayor
-                // cambiamos change a verdadero
-                change = true;
-                // intercambiamos los valores
+                change = true; // si es mayor cambiamos change a verdadero
+
+
+                intercambios++;
                 swap(list, j, j+1);
             }
         }
     }
 }
 
-// Selection Sort
 template <typename T>
 void selectionSort(vector<T> &list) {
-    // iteramos toda la lista desde el principio hasta uno antes del final
+   
     for (int i=0; i<list.size()-1; i++) {
-        // hacemos el indice de la posición i como el más chico
-        int min = i;
-        // iteramos desde el siguiente índice hasta el final
-        for (int j=i+1; j<list.size(); j++) {
-            // comparamos el valor de j contra min
-            if (list[j] < list[min]) {
-                // si es menor
-                // Actualizamos el valor de min
-                min = j;
+        
+        int min = i;// hacemos el indice de la posición i como el más chico
+        
+        for (int j=i+1; j<list.size(); j++) { // iteramos desde el siguiente índice hasta el final
+           
+
+
+            comparaciones++;
+            if (list[j] < list[min]) {  // comparamos el valor de j contra min
+                
+                min = j; // si es menor Actualizamos el valor de min
             }
         }
-        // intercambiamos el valor de min por el valor de i
-        swap(list, min, i);
+        intercambios++;
+        swap(list, min, i); // intercambiamos el valor de min por el valor de i y hasta ahora acabamos un ciclo del gran for.
     }
 }
 
-// Selection Sort
+
 template <typename T>
 void insertionSort(vector<T> &list) {
-    // iteramos la lista desde la segunda posición hasta el final
+    
     for (int i=1; i<list.size(); i++) {
         // iteramos desde i hasta 0
         bool exit = false;
         for (int j=i; j>0 && !exit; j--) {
+
+            comparaciones++;
             if (list[j] < list[j-1]) {
+
+
+                intercambios++;
                swap(list, j, j-1); 
             } else {
                 exit = true;
             }
         }
-        // declaramos un contador para ver donde vamos  
-        // int j = i;
-        // while (j>0 && list[j] < list[j-1]) {
-        //     // Intercambiamos j con j-1
-        //     swap(list, j, j-1);
-        //     // decrementamos j
-        //     j--;
-        // }
+
     }
 }
 
 template <typename T>
 int getPivot(vector<T> &list, int left, int right) {
-    // creamos una variable auxiliar con el valor de left - 1
-    int aux = left - 1;
-    // creamos una variable pivot con el valor de right
-    int pivot = right;
-    // iteramos desde left hasta pivot - 1 (< pivot)
+
+    int aux = left - 1;     // creamos una variable auxiliar con el valor de left - 1 para que vaya al siguiente elemento
+
+    int pivot = right;  // creamos una variable pivot con el valor de right para usarla como referencia
+
     for (int i=left; i<pivot; i++) {
-        // comparamos si el valor de pivot > al valor de i
+       
+
+
+        comparaciones++;
         if (list[pivot] > list[i]) {
-            // incrementamos el valor de i
-            aux++;
-            // intercambiamos aux con i
-            swap(list, aux, i);
+            aux++; // incrementamos el valor de i para que vaya al siguiente elemento
+
+            intercambios++;
+            swap(list, aux, i); // intercambiamos aux con i
         }
     }
-    // incrementamos aux
-    aux++;
-    // intercambiamos aux con pivot
-    swap(list, aux, pivot);
-    // regresamos aux
+
+    aux++;  // se incrementa aux para que vaya al siguiente elemento
+
+    intercambios++;
+    swap(list, aux, pivot); // intercambiamos aux con pivot
+   
     return aux;
 }
 
 template <typename T>
 void quickSort(vector<T> &list, int left, int right) {
-    // se ejectua la recursividad si left < right
+
+
+    comparaciones++;
     if (left < right) { // condición de control
         int pivot = getPivot(list, left, right);
         // ordenamos la lista del lado izq de pivote
@@ -159,6 +194,8 @@ void merge(vector<T> &list, int left, int mid, int right) {
     int j=0;
     // iteramos mientras no se acaben las listas
     while (i<leftList.size() && j<rightList.size()) {
+
+        comparaciones++;
         // comparamos el valor de i de la listaizq con el valor de j de la lista der
         if (leftList[i] < rightList[j]) {
             // actualizamos list en index con el valor de listLeft en i
@@ -196,16 +233,18 @@ void merge(vector<T> &list, int left, int mid, int right) {
 
 template <typename T>
 void mergeSort(vector<T> &list, int left, int right) {
-    // la condición de control es que left < right
-    if (left < right) {
-        // calculamos mid
+
+    comparaciones++;
+
+    if (left < right) { // la condición de control 
+     
         int mid = (left + right) / 2;
-        // ordenamos de left a mid 
-        mergeSort(list, left, mid);
-        // ordenamos de mid+1 a right 
-        mergeSort(list, mid+1, right);
-        // combinamos las dos partes de la lista
-        merge(list, left, mid, right);
+
+        mergeSort(list, left, mid);  // ordenamos de left a mid con recursividad
+
+        mergeSort(list, mid+1, right); // ordenamos de mid+1 a right
+
+        merge(list, left, mid, right); //usamos el merge para combinar las dos partes
     }
 }
 
@@ -218,41 +257,243 @@ void print(vector<T> &list) {
     cout << endl;
 }
 
+vector<int> generarListaInt(int cantidad) {
+    vector<int> lista;
+    for (int i = 0; i < cantidad; i++) {
+        lista.push_back(rand() % 100000);
+    }
+    return lista;
+}
+
+vector<float> generarListaFloat(int cantidad) {
+    vector<float> lista;
+    for (int i = 0; i < cantidad; i++) {
+        lista.push_back((float)(rand() % 100000) / 100.0);
+    }
+    return lista;
+}
+
+vector<char> generarListaChar(int cantidad) {
+    vector<char> lista;
+    for (int i = 0; i < cantidad; i++) {
+        lista.push_back((char)(rand() % 26 + 65)); // letras A-Z
+    }
+    return lista;
+}
+
 int main() {
+    srand(time(0));
 
-    vector<int> list = {15, 7, 3, 9, 12, 5, 2};
-    vector<int> listOriginal = list;
-    cout << "Lista original: " << endl;
-    print(list);
-    swapSort(list);
-    cout << "Lista ordenada: con Swap Sort" << endl;
-    print(list);
-    list = listOriginal;
-    print(list);
-    bubbleSort(list);
-    cout << "Lista ordenada: con Bubble Sort" << endl;
-    print(list);
-    list = listOriginal;
-    print(list);
-    selectionSort(list);
-    cout << "Lista ordenada: con Selection Sort" << endl;
-    print(list);
-    list = listOriginal;
-    print(list);
-    insertionSort(list);
-    cout << "Lista ordenada: con Insertion Sort" << endl;
-    print(list);
-    cout << "Último elemento: " << list[-1] << endl;
-    list = listOriginal;
-    print(list);
-    quickSort(list, 0, list.size()-1);
-    cout << "Lista ordenada: con Quick Sort" << endl;
-    print(list);
-    list = listOriginal;
-    print(list);
-    mergeSort(list, 0, list.size()-1);
-    cout << "Lista ordenada: con Merge Sort" << endl;
-    print(list); 
+    // 9 listas: 3 tipos x 3 tamanos
+    vector<int>   listaInt1000,   listaInt10000,   listaInt100000;
+    vector<float> listaFloat1000, listaFloat10000, listaFloat100000;
+    vector<char>  listaChar1000,  listaChar10000,  listaChar100000;
 
+    // Bandera para saber si ya se crearon las listas
+    bool listasCreadas = false;
+
+    // Tabla para guardar los tiempos: [algoritmo][tipo][tamano]
+    // Se llena de ceros automaticamente
+    double tiempos[7][3][3] = {0};
+
+    int opcion;
+    do {
+        cout << "1. Crear listas aleatorias\n";
+        cout << "2. Swap Sort\n";
+        cout << "3. Bubble Sort\n";
+        cout << "4. Selection Sort\n";
+        cout << "5. Insertion Sort\n";
+        cout << "6. Merge Sort\n";
+        cout << "7. Quick Sort\n";
+        cout << "8. Shell Sort (extra)\n";
+        cout << "9. Analisis comparativo (todos x tipos x tamanos)\n";
+        cout << "0. Salir\n";
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        // === OPCION 1: crear listas ===
+        if (opcion == 1) {
+            cout << "Creando listas...\n";
+
+            // Listas de enteros
+            listaInt1000   = generarListaInt(1000);
+            listaInt10000  = generarListaInt(10000);
+            listaInt100000 = generarListaInt(100000);
+
+            // Listas de float
+            listaFloat1000   = generarListaFloat(1000);
+            listaFloat10000  = generarListaFloat(10000);
+            listaFloat100000 = generarListaFloat(100000);
+
+            // Listas de char
+            listaChar1000   = generarListaChar(1000);
+            listaChar10000  = generarListaChar(10000);
+            listaChar100000 = generarListaChar(100000);
+
+            listasCreadas = true;
+            cout << "Listas creadas con exito.\n";
+        }
+
+        // === OPCIONES 2 a 8: ordenar la lista de int de 1000 ===
+        else if (opcion >= 2 && opcion <= 8) {
+            if (!listasCreadas) {
+                cout << "Primero crea las listas (opcion 1).\n";
+                continue;
+            }
+
+            // Hacemos una copia para no perder la original
+            vector<int> copia = listaInt1000;
+
+            // Reiniciamos contadores
+            comparaciones = 0;
+            intercambios = 0;
+
+            string nombre = "";
+
+            // Tomamos el tiempo antes de ordenar
+            clock_t inicio = clock();
+
+            if (opcion == 2) { nombre = "SwapSort";      swapSort(copia); }
+            if (opcion == 3) { nombre = "BubbleSort";    bubbleSort(copia); }
+            if (opcion == 4) { nombre = "SelectionSort"; selectionSort(copia); }
+            if (opcion == 5) { nombre = "InsertionSort"; insertionSort(copia); }
+            if (opcion == 6) { nombre = "MergeSort";     mergeSort(copia, 0, copia.size()-1); }
+            if (opcion == 7) { nombre = "QuickSort";     quickSort(copia, 0, copia.size()-1); }
+            if (opcion == 8) { nombre = "ShellSort";     shellSort(copia); }
+
+            // Tomamos el tiempo despues de ordenar
+            clock_t fin = clock();
+            double segundos = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            double nanosegundos = segundos * 1000000000.0;
+
+            // Mostramos resultados
+            cout << "\n--- " << nombre << " ---\n";
+            cout << "Lista ordenada (primeros 20):\n";
+            cout << "Tiempo: " << nanosegundos << " ns\n";
+
+
+            if (opcion >= 2 && opcion <= 5) {
+                cout << "Comparaciones: " << comparaciones << "\n";
+                cout << "Intercambios: " << intercambios << "\n";
+            }
+        }
+
+        else if (opcion == 9) {
+            if (!listasCreadas) {
+                cout << "Primero crea las listas (opcion 1).\n";
+                continue;
+            }
+
+            string nombres[7] = {"SwapSort","BubbleSort","SelectionSort","InsertionSort", "MergeSort","QuickSort","ShellSort"};
+
+            for (int alg = 0; alg < 7; alg++) {
+                for (int tipo = 0; tipo < 3; tipo++) {
+                    for (int tam = 0; tam < 3; tam++) {
+
+        
+                        int cantidad = 1000;
+                        if (tam == 1) cantidad = 10000;
+                        if (tam == 2) cantidad = 100000;
+
+                        
+                        clock_t inicio = clock(); // Tomamos el tiempo antes
+
+                        if (tipo == 0) {
+                            vector<int> ci;
+                            if (tam == 0) ci = listaInt1000;
+                            if (tam == 1) ci = listaInt10000;
+                            if (tam == 2) ci = listaInt100000;
+
+                            if (alg == 0) swapSort(ci);
+                            if (alg == 1) bubbleSort(ci);
+                            if (alg == 2) selectionSort(ci);
+                            if (alg == 3) insertionSort(ci);
+                            if (alg == 4) mergeSort(ci, 0, ci.size()-1);
+                            if (alg == 5) quickSort(ci, 0, ci.size()-1);
+                            if (alg == 6) shellSort(ci);
+                        }
+
+                        else if (tipo == 1) {
+                            vector<float> cf;
+                            if (tam == 0) cf = listaFloat1000;
+                            if (tam == 1) cf = listaFloat10000;
+                            if (tam == 2) cf = listaFloat100000;
+
+                            if (alg == 0) swapSort(cf);
+                            if (alg == 1) bubbleSort(cf);
+                            if (alg == 2) selectionSort(cf);
+                            if (alg == 3) insertionSort(cf);
+                            if (alg == 4) mergeSort(cf, 0, cf.size()-1);
+                            if (alg == 5) quickSort(cf, 0, cf.size()-1);
+                            if (alg == 6) shellSort(cf);
+                        }
+                        else {
+                            vector<char> cc;
+                            if (tam == 0) cc = listaChar1000;
+                            if (tam == 1) cc = listaChar10000;
+                            if (tam == 2) cc = listaChar100000;
+
+                            if (alg == 0) swapSort(cc);
+                            if (alg == 1) bubbleSort(cc);
+                            if (alg == 2) selectionSort(cc);
+                            if (alg == 3) insertionSort(cc);
+                            if (alg == 4) mergeSort(cc, 0, cc.size()-1);
+                            if (alg == 5) quickSort(cc, 0, cc.size()-1);
+                            if (alg == 6) shellSort(cc);
+                        }
+
+                        // Tomamos el tiempo despues
+                        clock_t fin = clock();
+                        double segundos = (double)(fin - inicio) / CLOCKS_PER_SEC;
+                        double nanosegundos = segundos * 1000000000.0;
+
+                        // Guardamos el tiempo en la tabla
+                        tiempos[alg][tipo][tam] = nanosegundos;
+
+                        cout << "Hecho: " << nombres[alg] << " tipo " << tipo
+                        << " tam " << cantidad << " -> " << nanosegundos << " ns\n";
+                    }
+                }
+            }
+
+            string tipos[3] = {"int","float","char"};
+            cout << "\nalgoritmo,tipoDato,tiempo1000,tiempo10000,tiempo100000\n";
+            for (int alg = 0; alg < 7; alg++) {
+                for (int tipo = 0; tipo < 3; tipo++) {
+                    cout << nombres[alg] << "," << tipos[tipo] << ","
+                         << tiempos[alg][tipo][0] << ","
+                         << tiempos[alg][tipo][1] << ","
+                         << tiempos[alg][tipo][2] << "\n";
+                }
+            }
+        }
+
+    } while (opcion != 0);
+
+    cout << "bye";
     return 0;
 }
+
+
+//Reflexión sobre el uso de Copilot
+//1. ¿Qué algoritmos lograste implementar con mayor facilidad gracias a Copilot?
+
+//La verdad inguno fue tan fácil, los primeros cuatro (Swap, Bubble, Selection e Insertion) fueron más rápidos porque son más cortos cortos, pero aun así tuve que leer cada línea para entenderla Merge Sort y Quick Sort fueron los más difíciles por la recursividad y los índices que tengo que poner, copilot me ayudó mucho a completar cositas que no sabia que eran necesarias en mi codigo.
+
+//2. ¿En qué casos Copilot te dio soluciones correctas de inmediato y en cuáles tuviste que corregir o ajustar?
+
+//Cuando le pedí cosas específicas  sin mover o cambiar todo mi código porque aveces le pedia cosas pequeñas y me ponia código que no entendía o medir tiempo sin funciones raras, aveces tmb quitaba mis comments que puse para entender mejor todo, igual me gusta ir revisando linea a linea.
+
+//3. ¿Consideras que Copilot te ayudó ahorrar tiempo o te obligó a pensar más?
+
+//Las dos cosas Me ahorró tiempo escribiendo, pero me obligó a pensar más porque tuve que entender y corregir cada línea. Decidí usarlo para estudiar.
+
+//4. Ejemplo concreto donde Copilot no resolvió el problema directamente:
+
+//En todo el código fui teniendo yo la idea pero muchas veces copilot me ayudó a implementarla real y bien porque siempre me marcaba error y con correcta sintaxis ya que no siempre sabía cómo hacerlo o qué funciones usar..
+
+
+//5. ¿Qué hubieras hecho diferente sin Copilot?
+
+//La verdad en muchas situaciones no habría sabido como iniciar lo que quería hacer porque la idea ya la tenía pero no me sé muchas funciones o de que manera lo puedo lograr, pero habría razonado cada línea desde el principio. Lo que sí habría hecho igual es probar cada algoritmo con listas chiquitas antes de las grandes, porque eso me ayudó mucho a entender.
+//Copilot fue útil para avanzar más rápido, pero no reemplazó mi estudio ni mi razonamiento. Tuve que dedicarle tiempo a entender, corregir y ajustar cada parte.
