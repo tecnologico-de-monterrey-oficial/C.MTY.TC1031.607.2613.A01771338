ba752efd3ab3ed4a0fe12be4493fa5d60ff92bd7
ElenaaJordan <A01771338@tec.mx>
2026-09-14 20:22:45 -0600

    total logs

--- Evidences/Evidence1/src/main.cpp
+++ Evidences/Evidence1/src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <sstream> //nos deja separar en pedacitos el texto
 #include "../include/Log.h"
 #include <vector>
 
 
 int main() {
 
-    std::ifstream file("../data/log607-1.txt");
+    std::ifstream file("../data/log607-1.txt");     //lee el file 
 
     if (!file) {
         std::cout << "Error!! The file could no be opened.\n";
@@ -16,53 +16,55 @@ int main() {
     }
 
     
-    std::string line;
-    std::vector<Log> logs;
-    
-    std::getline(file, line);
+    std::string line;  
+    std::vector<Log> logs;  // Vector para almacenar muchos logs
 
-    std::stringstream ss(line); // Crea un stringstream a partir de la línea leída
+    while (std::getline(file, line)) { //mientras haya lineas que leer las lee
+        std::stringstream ss(line); 
 
-    std::string month;
-    int day;
-    int year;
-    std::string time;
-    std::string ip;
-    std::string message;
-    ss >> month >> day >> year >> time >> ip; // Extrae los datos de la línea sin importar sus espacios gracias a ss
 
-    std::getline(ss, message); // Extrae el mensaje completo
-    if (!message.empty() && message[0] == ' ') { //borra el espacio inicial
-        message.erase(0, 1);
+        //crear variables para convertir los pedacitos en un objeto Log
+        std::string month;
+        int day;
+        int year;
+        std::string time;
+        std::string ip;
+        std::string message;
+
+        ss >> month >> day >> year >> time >> ip; // Extrae los primeros datos de la línea "Toma la primera palabra y ponla en month; toma la siguiente y ponla en day"
+
+        std::getline(ss, message); // Toma el mensaje completo
+        if (!message.empty() && message[0] == ' ') { 
+        message.erase(0, 1);  //borra el espacio inicial
+        }
+
+        std::stringstream timeStream(time);
+        int hour;
+        int min;
+        int sec;
+        char colon; //guarda aparte el caracter ':'
+        timeStream >> hour >> colon >> min >> colon >> sec; 
+
+        
+        //crear log
+
+        Log event;
+        event.month = mesANumero(month);
+        event.day = day;
+        event.year = year;
+        event.hour = hour;
+        event.min = min;
+        event.sec = sec;
+        event.ip = ip;
+        event.message = message;
+
+        //guardar log
+        logs.push_back(event);
+
+        
+
     }
-
-    std::stringstream timeStream(time);
-
-    int hour;
-    int min;
-    int sec;
-    char colon;
-    timeStream >> hour >> colon >> min >> colon >> sec;
-
-    
-    Log event;
-    event.month = mesANumero(month);
-    event.day = day;
-    event.year = year;
-    event.hour = hour;
-    event.min = min;
-    event.sec = sec;
-    event.ip = ip;
-    event.message = message;
-
-    std::cout << "Month: " << event.month << "\n";
-    std::cout << "Day: " << event.day << "\n";
-    std::cout << "Year: " << event.year << "\n";
-    std::cout << "Hour: " << event.hour << "\n";
-    std::cout << "Min: " << event.min << "\n";
-    std::cout << "Sec: " << event.sec << "\n";
-    std::cout << "IP: " << event.ip << "\n";
-    std::cout << "Message: " << event.message << "\n";
+    std::cout << "Total logs: " << logs.size() << "\n";
 
     return 0;
 }
